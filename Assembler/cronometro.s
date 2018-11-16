b404aa71342bd2b25f3a127825c3cb3217ab7317
Jaminson Riascos <jami.r.m@hotmail.com>
2018-11-15 23:18:10 -0500

    full clock working

--- Assembler/cronometro.s
+++ Assembler/cronometro.s
@@ -4,29 +4,36 @@
 .text
 .globl main
 main:
-	li $t0, 25 #number of iterations
-	li $t1, 0
-	li $t2, 0
 
-	li $t3, 61 #number of iterations
-	li $t4, 0
+	
+	li $t0, 24 #number of iterations
+	li $t1, -1
+	li $t2, 0
+loop_horas:
+	beq $t1, $t0 end
+
+	li $t3, 60 #number of iterations
+	li $t4, -1
 	li $t5, 0
 
-	li $t6, 61 #number of iterations
+	addi $t1,$t1,1 # Adds 1 to counter
+
+	j loop_minutos 
+	
+
+loop_minutos:
+
+	beq $t4, $t3 loop_horas
+
+	li $t6, 60 #number of iterations
 	li $t7, 0
 	li $t8, 0
 
-loop_horas:
-	beq $t1, $t0 end
-	j loop_minutos 
-	addi $t1,$t1,1
-	j loop_horas
+	addi $t4,$t4,1 # Adds 1 to counter
 
-loop_minutos:
-	beq $t4, $t3 loop_horas
 	j loop_segundos 
-	addi $t4,$t4,1
-	j loop_minutos
+
+	
 
 loop_segundos:
 
